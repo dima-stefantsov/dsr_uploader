70e83f004da8011662cadc17dbcdf08b917af08c
Dima Stefantsov <dima@stefantsov.com>
2021-02-10 05:10:46 +0300

    fixed allow spaces in folder paths

--- enable_autoupload.bat
+++ enable_autoupload.bat
@@ -10,9 +10,9 @@ rem Remove old dsr_uploader service.
 
 rem Enable automatic background Direct Strike replays uploads to ds-rating.com
 "%cd%\bin\nssm\nssm.exe" install dsr_uploader "%cd%\bin\php\php.exe"
-"%cd%\bin\nssm\nssm.exe" set dsr_uploader AppParameters "%cd%\src\service.php"
+"%cd%\bin\nssm\nssm.exe" set dsr_uploader AppParameters """%cd%\src\service.php"""
 rem Service can't know current user SC2 folder path. Passing it into service app directory.
-"%cd%\bin\nssm\nssm.exe" set dsr_uploader AppDirectory "%USERPROFILE%"
+"%cd%\bin\nssm\nssm.exe" set dsr_uploader AppDirectory """%USERPROFILE%"""
 "%cd%\bin\nssm\nssm.exe" set dsr_uploader AppExit Default Restart
 "%cd%\bin\nssm\nssm.exe" set dsr_uploader AppStdout "%cd%\src\log_uploads.txt"
 "%cd%\bin\nssm\nssm.exe" set dsr_uploader AppStderr "%cd%\src\log_errors.txt"
@@ -24,5 +24,3 @@ rem Service can't know current user SC2 folder path. Passing it into service app
 "%cd%\bin\nssm\nssm.exe" set dsr_uploader Start SERVICE_AUTO_START
 "%cd%\bin\nssm\nssm.exe" set dsr_uploader Type SERVICE_WIN32_OWN_PROCESS
 "%cd%\bin\nssm\nssm.exe" start dsr_uploader
-
-rem pause
